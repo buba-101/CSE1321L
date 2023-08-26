a9012bcaa23da32ba0bcc56ae5b73e02c866fbc4
Abakareh Dukuray <117542016+buba-101@users.noreply.github.com>
2023-08-26 03:08:09 +0000

    sync

--- Assignment/Assignment2/Assignment2B.cpp
+++ Assignment/Assignment2/Assignment2B.cpp
@@ -15,6 +15,25 @@ using namespace std;
 int main()
 {
 
+    int hrs, min, sec, time;
+  
+  //input
+  cout << "Time in seconds: ";
+  cin >> time;
+
+  cout << "Converting...\n";
+
+  //hour
+  hrs = time / 3600;
+
+  //minutes
+  min = (time % 3600) / 60;
+
+  //seconds
+  sec = time %  60;
+
+  //display
+  cout << time << " seconds == " << hrs << "hour(s), " << min << "minute(s), " << sec << "second(s).";
 
 }
 
